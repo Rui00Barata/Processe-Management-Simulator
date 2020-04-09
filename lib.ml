829a89c78eb6ea3bb73749d2648b2cfe0d74599e
Rui00Barata <rui.barata@ubi.pt>
2020-04-09 14:48:42 +0100

    estruturas feitas

--- lib.ml
+++ lib.ml
@@ -1,3 +1,44 @@
-(*Biblioteca*)
+(**Biblioteca*)
+(*Modulos adicionais*)
+open Queue
+open String
 
-let Tempo = ref 0
+(*MEMORY MODEL*)
+type instruction = 
+{
+	ins : char;
+	n : int;
+	nome : string
+}
+
+(*PROCESSS CONTROL BLOCK*)
+type pcb = 
+{
+	nome : string; 				(*nome da primeira instrução*)
+	start : int;					(*endereço da primeira instrução*)
+	variavel : int;				(*valor da variável*)
+	pid : int;						(*PID*)
+	ppid : int;						(*PPID*)
+	prioridade : int;			(*Prioridade do programa*)
+	pc : int;							(*Program Counter*)
+	estado : int					(*Estaado do program: blocked,...*)
+}
+
+(*GESTO DE PROCESSOS*)
+let tempo = ref 0
+
+let cpu = ref 0
+
+let pcbtabela = Queue.create
+
+let prontos = Queue.create
+
+let blocked = Queue.create
+
+type running_state = 
+{
+	ind : int;						(*indice do PCBTabela*)
+	pid : int;						(*PID do processo em execução*)
+	pc : int							(*Program counter do processo*)
+}
+	
