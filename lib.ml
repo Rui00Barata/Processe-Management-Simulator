60badbeb4be6deebd5ece4ffd13dbb81e649260b
p_duarte <paulo.duarte@ubi.pt>
2020-04-13 18:11:36 +0100

    Esboço

--- lib.ml
+++ lib.ml
@@ -1,1 +1,43 @@
-(*Biblioteca*)
+(**Biblioteca*)
+(*Modulos adicionais*)
+open Queue
+open String
+
+(*MEMORY MODEL*)
+type instruction = 
+{
+	ins : char;
+	n : int;
+	nome : string
+}
+
+(*PROCESSS CONTROL BLOCK*)
+type pcb = 
+{
+	nome : string; 				(*nome do processo*)
+	start : int;					(*endereço da primeira instrução*)
+	variavel : int;				(*valor da variável*)
+	pid : int;						(*PID*)
+	ppid : int;						(*PPID*)
+	prioridade : int;			(*Prioridade do programa*)
+	pc : int;							(*Program Counter*)
+	estado : int					(*Estado do program: blocked,...*)
+}
+
+(*GESTOR DE PROCESSOS*)
+let tempo = ref 0
+
+let cpu = ref 0
+
+let pcbtabela = Queue.create
+
+let prontos = Queue.create
+
+let blocked = Queue.create
+
+type running_state = 
+{
+	ind : int;						(*indice do PCBTabela*)
+	pid : int;						(*PID do processo em execução*)
+	pc : int							(*Program counter do processo*)
+}
--- pcb.ml
+++ pcb.ml
@@ -1,1 +1,4 @@
 (*Process Control Block*)
+open Unix
+
+let process = [{nome = "progenitor";  start = 0; variavel = 0; pid = 0; ppid = 0; prioridade = 0; pc = 0; estado = 0}]
