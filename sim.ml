92bdc962aa646347e0af12808c6f7a31219bb309
rubenhg <ruben.henriques.guilherme@hotmail.com>
2020-04-15 11:41:50 +0100

    Added func: abrir, tirar_CR; Added var: memory_model, ultimo;

--- sim.ml
+++ sim.ml
@@ -2,25 +2,44 @@
 (* Modulos *)
 Open Lib
 
-(*let rec simular processo =
-  if (processo.estado = 2) then ()
-  if processo.pc != 0 then
-    begin
-    let aux = ref 0 in
-    while aux != processo.pc do
-      if ((input_char fi)=='\n') then aux := !aux + 1
-    done
-    end*)
-  
-let ins line =
-  let x = String.split_on_char ' ' line in
-  match x with
-  |[] -> {ins = char_of_int 0;n = 0;nome = ""}
-  |[hd] -> {ins = hd.[0];n = 0;nome = ""}
-  |h::t -> 
-    if h.[0] != 'L'  then {ins = h.[0];n = int_of_string (List.hd t);nome = ""}
-    else {ins = h.[0];n = 0;nome = (List.hd t)}
+(*Variáveis*)
+let memory_model = Array.make 1000 {ins = 'N'; n = 0; nome = ""}
+let ultimo = ref 0
 
-(*let () =
-  let fi = open_in processo.nome in
-  simular processo*)
+
+(*Funções*)
+let tirar_CR str =
+  let n = String.length str in
+  if n > 0 && str.[n-1] = '\r' then
+    String.sub str 0 (n-1)
+  else
+    str
+
+let line_to_instr line =
+  let x = String.split_on_char ' ' (tirar_CR line) in
+  match x with
+  |[] -> {ins = char_of_int 0; n = 0; nome = ""}
+  |[hd] -> {ins = hd.[0]; n = 0; nome = ""}
+  |h::t -> 
+    if h.[0] <> 'L'  then {ins = h.[0];n = int_of_string (List.hd t); nome = ""}
+    else if h.[0] = 'L' then {ins = h.[0];n = 0;nome = (List.hd t)}
+    else {ins = 'E';n = 0;nome = ""}
+
+
+let abrir path =
+  let fi = open_in path in
+  let flag = ref true in
+  let line = ref "" in
+  let instr = ref {ins = 'N'; n = 0; nome = ""} in
+  while (!flag) do
+      try 
+      begin 
+        line := input_line fi;
+        instr := line_to_instr !line;
+        memory_model.(!ultimo) <- !instr;
+        ultimo := !ultimo + 1 
+      end
+      with End_of_file -> begin line := "EOF"; close_in fi; flag := false end
+  done
+
+
